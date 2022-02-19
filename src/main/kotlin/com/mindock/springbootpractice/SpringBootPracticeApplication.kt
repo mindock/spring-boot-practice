babadeed9f62c00c4f8cef24b032769d01bd1731
MINDOCK <parksy8189@gmail.com>
2022-02-19 22:27:54 +0900

    🎉 프로젝트 시작

---
+++ src/main/kotlin/com/mindock/springbootpractice/SpringBootPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.mindock.springbootpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootPracticeApplication>(*args)
+}
