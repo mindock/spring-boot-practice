a9f5d57191b69e7ec21f8f197f2fc943b5c2947e
MINDOCK <parksy8189@gmail.com>
2022-02-26 23:29:54 +0900

    🔨 @EnableConfigurationProperties 생략
    - 참고: https://www.baeldung.com/configuration-properties-in-spring-boot#1-spring-boot-22

--- spring-boot-starter/src/main/kotlin/com/mindock/springbootstarter/config/PersonConfiguration.kt
+++ spring-boot-starter/src/main/kotlin/com/mindock/springbootstarter/config/PersonConfiguration.kt
@@ -1,18 +1,17 @@
 package com.mindock.springbootstarter.config
 
 import com.mindock.springbootstarter.domain.Person
-import com.mindock.springbootstarter.domain.PersonProperties
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableConfigurationProperties(PersonProperties::class)
 class PersonConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    fun person(properties: PersonProperties): Person =
-        Person(name = properties.name, age = properties.age)
+    @ConfigurationProperties("person")
+    fun person(): Person = Person()
 }
--- spring-boot-starter/src/main/kotlin/com/mindock/springbootstarter/domain/Person.kt
+++ spring-boot-starter/src/main/kotlin/com/mindock/springbootstarter/domain/Person.kt
@@ -1,6 +1,9 @@
 package com.mindock.springbootstarter.domain
 
-data class Person(
-    private val name: String,
-    private val age: Int,
-)
+class Person {
+    lateinit var name: String
+    lateinit var age: Integer
+
+    override fun toString(): String =
+        "Person(name='$name', age=$age)"
+}
