ddeab64fd54e7350fd87c826b307ab724e284197
MINDOCK <parksy8189@gmail.com>
2022-02-27 00:00:29 +0900

    🔨 spring-boot-starter 수정
    - application.properties 제거
    - spring-boot-configuration-processor 의존성 제거

--- spring-boot-starter/build.gradle.kts
+++ spring-boot-starter/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation("org.springframework.boot:spring-boot-autoconfigure-processor")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+//    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 }
