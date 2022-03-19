909c26a7afb7eafc03338a54b88083bc911f5202
MINDOCK <parksy8189@gmail.com>
2022-03-20 02:21:53 +0900

    ✨ 로깅

--- spring-application-init/build.gradle.kts
+++ spring-application-init/build.gradle.kts
@@ -2,6 +2,14 @@ dependencies {
     kapt("org.springframework.boot:spring-boot-configuration-processor")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+
+    modules {
+        module("org.springframework.boot:spring-boot-starter-logging") {
+            replacedBy("org.springframework.boot:spring-boot-starter-log4j2", "Use Log4j2")
+        }
+    }
+
     testImplementation("junit:junit:4.13.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- spring-application-init/src/main/kotlin/com/mindock/springapplicationinit/application/SecondApplicationRunner.kt
+++ spring-application-init/src/main/kotlin/com/mindock/springapplicationinit/application/SecondApplicationRunner.kt
@@ -1,6 +1,7 @@
 package com.mindock.springapplicationinit.application
 
 import com.mindock.springapplicationinit.config.ProjectOwnerProperties
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
@@ -13,15 +14,16 @@ class SecondApplicationRunner(
     private val projectOwnerProperties: ProjectOwnerProperties,
     private val hello: String,
 ): ApplicationRunner {
+    private val logger = LoggerFactory.getLogger(SecondApplicationRunner::class.java)
 
     override fun run(args: ApplicationArguments?) {
-        println("SECOND 222222222222222222")
-        println("=========================")
-        println("(properties) project owner's name is ${projectOwnerProperties.name}")
-        println("(properties) project owner's age is ${projectOwnerProperties.age}")
-        println("(properties) project owner's full name is ${projectOwnerProperties.fullName}")
-        println("(properties) project owner's session timeout is ${projectOwnerProperties.sessionTimeout}")
-        println("=========================")
-        println("hello bean: $hello")
+        logger.debug("SECOND 222222222222222222")
+        logger.debug("=========================")
+        logger.debug("(properties) project owner's name is ${projectOwnerProperties.name}")
+        logger.debug("(properties) project owner's age is ${projectOwnerProperties.age}")
+        logger.debug("(properties) project owner's full name is ${projectOwnerProperties.fullName}")
+        logger.debug("(properties) project owner's session timeout is ${projectOwnerProperties.sessionTimeout}")
+        logger.debug("=========================")
+        logger.debug("hello bean: $hello")
     }
 }
