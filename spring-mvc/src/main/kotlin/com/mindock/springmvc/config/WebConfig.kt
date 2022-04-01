e2c11bb1e457355781b00c2437b7a602f1c93035
MINDOCK <parksy8189@gmail.com>
2022-04-02 01:54:26 +0900

    ✨ 스프링 MVC: 정적 리소스

--- spring-mvc/src/main/kotlin/com/mindock/springmvc/config/WebConfig.kt
+++ spring-mvc/src/main/kotlin/com/mindock/springmvc/config/WebConfig.kt
@@ -1,8 +1,15 @@
 package com.mindock.springmvc.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/m/**")           // /m/ 으로 시작하는 요청이 오면,
+            .addResourceLocations("classpath:/mm/")      // mm 디렉토리 밑의 리소스를 제공함 (여기서, location은 /로 끝나야함)
+            .setCachePeriod(60)
+    }
 }
