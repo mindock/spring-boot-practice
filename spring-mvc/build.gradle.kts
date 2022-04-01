4d7d8791c5974b08e3e1b718a424665e2eff32ce
MINDOCK <parksy8189@gmail.com>
2022-04-02 02:05:53 +0900

    ✨ 스프링 MVC: webjar

--- spring-mvc/build.gradle.kts
+++ spring-mvc/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
+    implementation("org.webjars.bower:jquery:3.6.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
